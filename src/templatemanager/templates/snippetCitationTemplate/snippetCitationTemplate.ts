514402ee1dd0f3cb57b1101c307d519f30a2923e
Bharadwaj.Adapala@ki0849 <bhardawaj.adapala@kore.com>
2023-09-12 15:44:41 +0530

    updated Templates

---
+++ src/templatemanager/templates/snippetCitationTemplate/snippetCitationTemplate.ts
@@ -0,0 +1,132 @@
+
+import helpers from '../../../utils/helpers';
+import './snippetCitationTemplate.scss';
+import FeedBackFormTemplate from '../../templates/feedBackFormTemplate/feedBackFormTemplate';
+class SnippetCitationTemplate {
+    renderMessage(msgData: any) {
+        let me: any = this;
+        let $ = me.hostInstance.$;
+        let helpersObj = helpers;
+
+        if (msgData?.message?.[0]?.component?.payload?.template_type === "citation_snippet") {
+            me.messageHtml = $(SnippetCitationTemplate.prototype.getTemplateString()).tmpl({
+                'snippetData': msgData?.message?.[0]?.component?.payload?.snippetData,
+                'helpers': helpersObj.helpers,
+                'displayFeedback':msgData?.message?.[0]?.component?.payload?.feedbackDisplay
+            });
+            me.feedBackTemplateObj = new FeedBackFormTemplate();
+            setTimeout(()=>{
+              SnippetCitationTemplate.prototype.bindSnippetEvents(me, me.messageHtml,msgData?.message?.[0]?.component?.payload?.snippetData);
+            },500)
+            return me.messageHtml;
+        }
+    }
+    getTemplateString() {
+        var snipppetCitationTemplate  = '<script type="text/x-jqury-tmpl">\
+        <div class="search-temp-one">\
+        <div class="top-header">\
+            <div class="top-header-with-img">\
+                <span class="logo-span"><img src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/snippet_imgs/snippet-avathar.svg"/></span>\
+                <div class="btn-chip"><span class="sdk-i18n-lang" sdk-i18n-key="sa_sdk_Suggested_answer">{{html langTranslator("sa_sdk_Suggested_answer")}}</span>\</div>\
+            </div>\
+            {{if snippetData && snippetData.snippet_type === "generative_model"}}\
+            <div class="btn-link"><span class="bot-bg-purple"><img src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/snippet_imgs/bot.svg"/></span><span class="sdk-i18n-lang" sdk-i18n-key="sa_sdk_answered_by_ai">{{html langTranslator("sa_sdk_answered_by_ai")}}</span>\</div>\
+            {{/if}}\
+        </div>\
+        {{if snippetData && snippetData.title}}\
+        <div class="img-temp-title sa-sdk-title" data-title="{{html helpers.convertMDtoHTML(snippetData?.title)}}">{{html helpers.convertMDtoHTML(snippetData?.title)}}</div>\
+        {{/if}}\
+        <div class="citation-data-desc  {{if snippetData.title==""}}snippet_padding_top_0{{/if}}">\
+        {{each(key, data) snippetData.answer}}\
+        <span class="snippet-answer-fragment">{{html data.answer_fragment}}</span>{{each(sourceKey, source) data.sources}}<sup class="snippet-citation"><a href="${source.url}" target="_blank">[${source._id}]</a></sup>{{/each}}. </span>\
+        {{/each}}\
+        </div>\
+        <div class="snippet-referene-block">\
+          <div class="reference-block-header">References: </div>\
+          <ol type="1" class="reference-list-temp-ul">\
+                  {{each(key, item) snippetData.reference}}\
+                      <li class="reference-list-temp-li" title="{{html helpers.convertMDtoHTML(item.title)}}" ><a  href="${item.url}" target="_blank"><span>{{html helpers.convertMDtoHTML(item.title)}}</span></a></li>\
+                      {{/each}}\
+                  </ol>\
+        </div>\
+        {{if snippetData && snippetData.source}}\
+        <div class="snippet-source-block">\
+          <div class="snippet-source-file-name {{if !snippetData.source}} display-none{{/if}}">{{html snippetData.source}}</div>\
+          <a href="${snippetData?.page_url}" target="_blank" ><div class="snippet-source-url {{if !snippetData.page_url}} display-none{{/if}}"><span class="snippet-source-url-name" title="${snippetData?.page_url}">${snippetData?.page_url}</span><img src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/Icons/external-link.svg"/></div></a>\
+        </div>\
+        {{/if}}\
+        <div class="temp-footer-block">\
+            <div class="temp-footer {{if snippetData && snippetData.snippet_type !== "generative_model"}} justify-content-end {{/if}}">\
+                {{if snippetData && snippetData.snippet_type === "generative_model"}}\
+                <div class="btn-link"><span class="bot-bg-purple"><img src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/snippet_imgs/bot.svg"/></span><span class="sdk-i18n-lang" sdk-i18n-key="sa_sdk_answered_by_ai">{{html langTranslator("sa_sdk_answered_by_ai")}}</span>\</div>\
+                {{/if}}\
+                {{if displayFeedback}}\
+                <div class="temp-right">\
+                    <div class="is-it-usefull">Is it useful?</div>\
+                    <div class="temp-fotter-actions">\
+                        <img  class="snippet-feedback  snippet-like-img" src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/snippet_imgs/like-gray.svg" />\
+                        <img class="snippet-feedback  snippet-dislike-img" src="https://koregeneric.s3.amazonaws.com/SearchAssist_UI_Img/snippet_imgs/dislike-gary.svg" />\
+                    </div>\
+                </div>\
+                <div id="snippet-feedback-template"></div>\
+                {{/if}}\
+            </div>\
+        </div>\
+    </div>\
+      </script>';
+        return snipppetCitationTemplate;
+    }
+    bindSnippetEvents(me:any,messageHtml:any,snippetData:any){
+      let $ = me.hostInstance.$;
+      let hostInstance= me.hostInstance;
+      $(messageHtml).find('.temp-fotter-actions').off('click', '.snippet-like-img').on('click', '.snippet-like-img', function (event:any) {
+        if(!$(event.currentTarget).closest('.snippet-like-img').hasClass('active')){
+        hostInstance.updateFeedBackResult('thumbsUp',snippetData.searchQuery,'smartAnswer')
+        $(messageHtml).find('.snippet-feedback').removeClass('active');
+        $(event.currentTarget).addClass('active');
+      }
+      });
+
+      $(messageHtml).find('.temp-fotter-actions').off('click', '.snippet-dislike-img').on('click', '.snippet-dislike-img', function (event:any) {
+        if(!$(event.currentTarget).closest('.snippet-dislike-img').hasClass('active')){
+          SnippetCitationTemplate.prototype.appendFeedBaackData(me,messageHtml,snippetData)
+        $(messageHtml).find('.snippet-feedback').removeClass('active');
+        $(event.currentTarget).addClass('active');
+      }
+      });
+        SnippetCitationTemplate.prototype.tooltipBindEvent(me);
+      }
+      tooltipBindEvent(me:any){
+        let $ = me.hostInstance.$;
+      $('.sa-sdk-title').off('mouseover').on('mouseover',function(e:any){
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+        $(e.currentTarget).before('<div class="sdk-tooltip-container">'+$(e.currentTarget).attr('data-title')+'<span class="sa-tooltip-arrow"></span></div>');
+        $(e.currentTarget).parent().find('.sdk-tooltip-container').css('top',($(e.currentTarget).position().top-($(e.currentTarget).parent().find('.sdk-tooltip-container').height()+25))+'px');
+      })
+      $('.sa-sdk-title').off('mouseout').on('mouseout',function(e:any){
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+        $(e.currentTarget).parent().find('.sdk-tooltip-container').remove();
+        })
+      }
+      appendFeedBaackData(me: any, messageHtml: any,snippetData:any){
+        let $ = me.hostInstance.$;
+        let feedbackMsgData = {
+          message: [{
+            component: {
+              type: 'template',
+              payload: {
+                template_type: "feedbackFormTemplate",
+                query: snippetData.searchQuery,
+                feedBackType:'smartAnswer'
+              }
+            }
+          }]
+        };
+          $(messageHtml).find('#snippet-feedback-template').empty().append(me.feedBackTemplateObj.renderMessage.bind(me, feedbackMsgData));
+       }
+}
+
+
+export default SnippetCitationTemplate;
