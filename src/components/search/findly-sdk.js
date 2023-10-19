cd7c691e0b085f0e042836071562c1267fe6d12d
Bharadwaj.Adapala@ki0849 <bhardawaj.adapala@kore.com>
2023-10-19 10:51:35 +0530

    merge conflicts resolved

--- src/components/search/findly-sdk.js
+++ src/components/search/findly-sdk.js
@@ -21722,17 +21722,12 @@ FindlySDK.prototype.getMergedData = function (settingData, responseData, searchT
             item.label2 = obj[mapping.label2];
           }
           if(obj[mapping.rating]){
-<<<<<<< Updated upstream
             item.price = obj[mapping.rating];
           }
           if(obj[mapping.rateField]){
             item.rating = obj[mapping.rating];
           }
           
-=======
-            item.rating = obj[mapping.rating];
-          }
->>>>>>> Stashed changes
           if(obj[mapping.strikedOffRate]){
             item.strike_off = obj[mapping.strikedOffRate];
           }
