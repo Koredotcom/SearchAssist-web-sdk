6daf31f5d3fe81f1c69b16cb01a0c1838b14dbd1
SujithPu-Kore <sujithkumar.puligilla@kore.com>
2024-07-23 19:42:27 +0530

    answer generation errors changes for bottomup

--- src/components/search/config/findly-config.ts
+++ src/components/search/config/findly-config.ts
@@ -11,19 +11,19 @@ declare global {
 let botOptionsFindly: any = {};
 botOptionsFindly.logLevel = "debug";
 var serverUrl = window.location.href;
-var paramUrl="searchassist.kore.ai";
+var paramUrl="searchassist-qa.kore.ai";
 var httpStart = 'https://';
 var wssUrl = "wss";
-if(serverUrl && (serverUrl.includes("https://") || serverUrl.includes("http://"))){
-paramUrl=serverUrl.split('/')[2];
-if(serverUrl.includes("https://")){
-httpStart = "https://";
-wssUrl = "wss";
-}else{
-httpStart = "http://";
-wssUrl = "ws";
-}
-}  
+// if(serverUrl && (serverUrl.includes("https://") || serverUrl.includes("http://"))){
+// paramUrl=serverUrl.split('/')[2];
+// if(serverUrl.includes("https://")){
+// httpStart = "https://";
+// wssUrl = "wss";
+// }else{
+// httpStart = "http://";
+// wssUrl = "ws";
+// }
+// }  
 if(window?.JWT_OBJ && window?.JWT_OBJ?.koreAPIUrl){
   paramUrl=window.JWT_OBJ.koreAPIUrl.split("/")[2].split(':')[0];
     if(window.JWT_OBJ.koreAPIUrl.includes("https://")){
@@ -96,7 +96,7 @@ findlyConfig = {
     showTaskMenuPickerIcon: true, //set true to show TaskMenu Template icon
     showradioOptionMenuPickerIcon: false, //set true to show Radio Option Template icon
   },
-  API_KEY_CONFIG:{'KEY':"YOUR_API_KEY"}
+  API_KEY_CONFIG:{'KEY':"a2107d9030444844aca511484918c11195f687166aa0462eb9fe2a7f1a9907e2st03"}
 };
 
 export default findlyConfig;
