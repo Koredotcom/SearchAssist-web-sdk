279bfb29c18510c68776499dcc9183cb4b60e81b
VenkatIc-Kore <78073146+VenkatIc-Kore@users.noreply.github.com>
2025-07-29 16:04:54 +0530

    sdk t systems See more Results not appearing for Sharepoint data issue fixed (#32)

--- src/components/search/findly-sdk.js
+++ src/components/search/findly-sdk.js
@@ -21373,6 +21373,8 @@ FindlySDK.prototype.countTotalResults = function (res, totalResultsCount) {
       availableGroups.forEach((group) => {
         if (!(res.tabFacet?.buckets && res.tabFacet?.buckets?.length)) {
           totalResultsCount = totalResultsCount + res.results[group].doc_count;
+        }else if((res.tabFacet?.buckets && res.tabFacet?.buckets?.length) && _self.vars.totalNumOfResults === 0){
+          totalResultsCount = totalResultsCount + res.results[group].doc_count;
         }
       });
     }
